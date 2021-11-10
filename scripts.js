247e330952e64878ece930a161f21c91d4a19196
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2021-11-10 12:34:07 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 $("#customerName").text("Shiv Kumar");
 $("#policyNumber").text("D037673294/25102021");
 $("#startDate").text("21-Oct-2021");
-$("#endDate").text("200-Oct-2022");
+$("#endDate").text("20-Oct-2022");
 
 
 $("#type").text("Two Wheeler");
