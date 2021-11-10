86fe3d1034080238c5aaf88bd0279d07944520cc
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2021-11-10 12:20:29 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 $("#customerName").text("Shiv Kumar");
-$("#policyNumber").text("D007742103");
-$("#startDate").text("11-Sep-2020");
-$("#endDate").text("10-Sep-2021");
+$("#policyNumber").text("D037673294/25102021");
+$("#startDate").text("21-Oct-2021");
+$("#endDate").text("200-Oct-2022");
 
 
 $("#type").text("Two Wheeler");
 $("#registrationNumber").text("PB08CZ5618");
 $("#make").text("BAJAJ");
-$("#model").text("DISCOVER M");
+$("#model").text("DISCOVER 125M");
 $("#vehicalSubType").text("DRUM BRAKE");
 $("#engineNo").text("PAZRFK99083");
 $("#chassis").text("MD2A57BZXFRK78669");
